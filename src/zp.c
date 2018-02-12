312cd9616ee115e64bcb1b52b7dd2906e123ca11
Michal Vyskocil <michal.vyskocil@gmail.com>
2018-02-12 20:19:19 +0100

    Problem: no project structure for zp
    Solution: generate via zproject

---
+++ src/zp.c
@@ -0,0 +1,48 @@
+/*  =========================================================================
+    zp - description
+
+    Copyright (c) the Contributors as noted in the AUTHORS file.
+    This file is part of ZP, ZeroMQ Piper
+    http://githut.com/zmonit/zp
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+    =========================================================================
+*/
+
+/*
+@header
+    zp -
+@discuss
+@end
+*/
+
+#include "zp_classes.h"
+
+int main (int argc, char *argv [])
+{
+    bool verbose = false;
+    int argn;
+    for (argn = 1; argn < argc; argn++) {
+        if (streq (argv [argn], "--help")
+        ||  streq (argv [argn], "-h")) {
+            puts ("zp [options] ...");
+            puts ("  --verbose / -v         verbose test output");
+            puts ("  --help / -h            this information");
+            return 0;
+        }
+        else
+        if (streq (argv [argn], "--verbose")
+        ||  streq (argv [argn], "-v"))
+            verbose = true;
+        else {
+            printf ("Unknown option: %s\n", argv [argn]);
+            return 1;
+        }
+    }
+    //  Insert main code here
+    if (verbose)
+        zsys_info ("zp - ");
+    return 0;
+}
